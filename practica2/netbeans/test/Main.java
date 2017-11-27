cefd5e9e2b6b77f4cbe4a72feb5ce76f8c5962f6
alex <ajah1@alu.ua.es>
2017-11-27 11:57:02 +0100

    estructura codigo

---
+++ practica2/netbeans/test/Main.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author alex
+ */
+public class Main {
+    
+    // prueba clasificador ceros
+    int totalImagenes = 97;
+    // cantidad de imágeneº para el aprendizaje
+    int aprender = 0;
+    // cantidad casos de prueba
+    int testear = totalImagenes - aprender;
+    
+    // cantidad de de clasificadores/umbrales [C]
+    int C = 0;
+    // valor del [+,-]umbral alteorio [0,783]
+    int umbralAlateorio = 0;
+    // mejor umbral
+    int bestClasificador = 0;
+
+    
+    for ( int i = 0; i < aprender; ++i )
+    {
+    
+    }
+}
